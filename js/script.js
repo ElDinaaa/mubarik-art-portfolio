52dc88b89edf9fc4de4471dcc8bbf79c0552ea52
Dina El <dina.tarantino@gmail.com>
2024-06-07 13:52:39 +0300

    Add animation for header and navigation on page load

--- js/script.js
+++ js/script.js
@@ -11,4 +11,20 @@ document.addEventListener('DOMContentLoaded', () => {
             document.body.style.overflow = 'auto' ;
         }
     });
+
+    const header = document.querySelector('.overlay h1');
+    const navItems = document.querySelectorAll('.navigation a');
+
+    header.style.opacity = 0;
+    navItems.forEach(link => link.style.opacity = 0);
+
+    setTimeout(() => {
+        header.style.transition = 'opacity 1s';
+        header.style.opacity = 1;
+        navItems.forEach((link, index) => {
+            link.style.transition = 'opasity 1s ${index * 0.2}s';
+            link.style.opacity = 1;
+        });
+    }, 100);
+
 });
