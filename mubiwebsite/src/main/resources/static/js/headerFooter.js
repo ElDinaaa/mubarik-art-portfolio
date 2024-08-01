b94decda2813b7605168eba0a864c282f78fc0a5
Dina El <dinael@Dinas-MacBook-Pro.local>
2024-08-01 18:05:06 +0300

    feat: update home page design of english and russian versions of the website. Start update of the Work page

--- mubiwebsite/src/main/resources/static/js/headerFooter.js
+++ mubiwebsite/src/main/resources/static/js/headerFooter.js
@@ -10,11 +10,11 @@ class SpecialHeader extends HTMLElement {
                     <li><a href="/en/work">Portfolio</a></li>
                     <li><a href="/en/about">About me</a></li>
                     <li><a href="/en/contact">Contact</a></li>
-                    <li class="language-switcher">
-                        <a href="/en/index" id="lang-en">En</a> | <a href="/ru/index" id="lang-ru">Рус</a>
-                    </li>
                 </ul>
             </nav>
+            <div class="language-switcher">
+                <a href="/en/index" id="lang-en">en</a> | <a href="/ru/index" id="lang-ru">ru</a>
+            </div>
         </div>
         <div class="header-line"></div>`;
     }
@@ -27,18 +27,18 @@ class SpecialHeaderRU extends HTMLElement {
         this.innerHTML = `
         <div class="header-container">
             <div class="logo">
-                <a href="/ru/index">Мубарик Эльмурзаева</a>
+                <a href="/ru/index">Mubarik Elmurzaeva</a>
             </div>
             <nav class="navigation">
                 <ul>
                     <li><a href="/ru/work">Портфолио</a></li>
                     <li><a href="/ru/about">Обо мне</a></li>
                     <li><a href="/ru/contact">Контакты</a></li>
-                    <li class="language-switcher">
-                        <a href="/en/index" id="lang-en">En</a> | <a href="/ru/index" id="lang-ru">Рус</a>
-                    </li>
                 </ul>
             </nav>
+            <div class="language-switcher">
+                <a href="/en/index" id="lang-en">en</a> | <a href="/ru/index" id="lang-ru">ru</a>
+            </div>
         </div>
         <div class="header-line"></div>`;
     }
